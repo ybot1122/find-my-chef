6a01bf4fd4bb942bc52d8e5c226a064d54342d01
Toby Liu <ybot1122@gmail.com>
2023-06-18 09:12:54 -0700

    Add Craco config for custm webpack for module scope plugin

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,23 @@
 const { VanillaExtractPlugin } = require("@vanilla-extract/webpack-plugin");
+const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
 
 module.exports = {
   webpack: {
     plugins: {
       add: [new VanillaExtractPlugin()],
     },
+    configure: (webpackConfig) => {
+        const moduleScopePlugin = webpackConfig.resolve.plugins.find(
+            (plugin) => plugin instanceof ModuleScopePlugin
+          );
+          moduleScopePlugin.allowedPaths.push(
+            path.resolve(
+              __dirname, 
+              "node_modules/@vanilla-extract/webpack-plugin"
+            )
+          );
+    
+          return webpackConfig;
+    }
   },
 };
