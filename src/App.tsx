7c4b7f6fd6d89f0ce226ae618b6590dec9830882
Toby Liu <ybot1122@gmail.com>
2023-06-18 08:52:10 -0700

    Create vanilla extract theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { themeClass } from "./theme.css";
+import * as css from "./App.css";
+
 const App = () => {
   return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    <div className={themeClass}>
+      <h1 className={css.title}>Hello World</h1>
+    </div>  );
 }
 
 export default App;
