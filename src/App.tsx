39c03bfd6182d1908a62b43415d66773a50c6b15
Toby Liu <ybot1122@gmail.com>
2023-06-18 09:29:42 -0700

    Add App.css.ts back in

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import { themeClass } from "./theme.css";
+import * as css from "./App.css";
 
 const App = () => {
   return (
     <div className={themeClass}>
-      <h1 className={''}>Hello World</h1>
-    </div>  );
+      <h1 className={css.title}>Hello World</h1>    </div>  );
 }
 
 export default App;
