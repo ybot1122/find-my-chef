8649c9fe1eca951dee2270d4535b0398b97b291f
Toby Liu <ybot1122@gmail.com>
2023-06-19 11:15:13 -0700

    Move copy text to dedicated variable

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { themeClass } from "./theme.css";
 import * as css from "./App.css";
 import classNames from "classnames";
 
+const FRONT_COVER_TEXT = 'Introducing a New Platform for Home Chefs';
+const FRONT_COVER_SUBTEXT = 'Finally, a digital platform optimized for home chefs to share and collaborate.'
+
 const App = () => {
   return (
     <div className={classNames(themeClass, css.app)}>
@@ -13,10 +16,10 @@ const App = () => {
       <body>
         <div id="front-cover" className={css.frontCover}>
           <div className={css.frontCoverText}>
-              Introducing a New Platform for Home Chefs
+              {FRONT_COVER_TEXT}
           </div>
           <p className={css.frontCoverSubtext}>
-            Finally, a digital platform optimized for home chefs to share and collaborate.
+            {FRONT_COVER_SUBTEXT}
           </p>
         </div>
       </body>
