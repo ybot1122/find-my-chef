fea45ed6e845d05f0f4aba54dc41d0590feceda9
Toby Liu <ybot1122@gmail.com>
2023-06-19 13:34:01 -0700

    Move front page cover to standalone directory

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import React from "react";
 import { themeClass } from "./theme.css";
 import * as css from "./App.css";
 import classNames from "classnames";
-
-const FRONT_COVER_INTRO = "Introducing";
-const FRONT_COVER_TEXT = "A New Platform";
-const FRONT_COVER_TEXT2 = "for Home Chefs";
-const FRONT_COVER_SUBTEXT =
-  "Finally, a digital platform optimized for home chefs to share and collaborate.";
+import { FrontPageCover } from "./components/frontPageCover/frontPageCover";
 
 const App = () => {
   return (
@@ -17,21 +12,7 @@ const App = () => {
         <h1>FindMyChef</h1>
       </header>
       <body>
-        <div id="front-cover" className={css.frontCover}>
-          <img
-            src="/pancakes.gif"
-            alt={FRONT_COVER_SUBTEXT}
-            className={css.frontCoverImage}
-          />
-          <div className={css.frontCoverText}>{FRONT_COVER_INTRO}</div>
-          <div className={css.breakRow} />
-          <div className={css.frontCoverText}>{FRONT_COVER_TEXT}</div>
-          <div className={css.breakRow} />
-          <div className={css.frontCoverText}>{FRONT_COVER_TEXT2}</div>
-          <div className={css.breakRow} />
-          <div className={css.spacer} />
-          <div className={css.frontCoverSubtext}>{FRONT_COVER_SUBTEXT}</div>
-        </div>
+        <FrontPageCover id="Introducing-A-New-Platform" />
       </body>
     </div>
   );
---
+++ src/components/frontPageCover/frontPageCover.css.ts
@@ -0,0 +1,54 @@
+import { style } from "@vanilla-extract/css";
+import { vars } from "../../theme.css";
+
+export const frontCover = style({
+  position: "relative",
+  display: "flex",
+  flexWrap: "wrap",
+  alignItems: "center",
+  justifyContent: "center",
+  textAlign: "center",
+  width: "100%",
+  margin: 0,
+  padding: "50px 0",
+});
+
+export const frontCoverScrim = style({
+  zIndex: 1,
+  backgroundColor: vars.colors.accent,
+  opacity: 0.6,
+});
+
+export const frontCoverImage = style({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+  zIndex: 0,
+});
+
+export const frontCoverText = style({
+  maxWidth: "768px",
+  fontSize: "4em",
+  zIndex: 2,
+});
+
+export const frontCoverSubtext = style({
+  width: "768px",
+  fontSize: "2em",
+  zIndex: 2,
+});
+
+export const breakRow = style({
+  flexBasis: "100%",
+  height: 0,
+  zIndex: 2,
+});
+
+export const spacer = style({
+  flexBasis: "100%",
+  height: "50px",
+  zIndex: 2,
+});
