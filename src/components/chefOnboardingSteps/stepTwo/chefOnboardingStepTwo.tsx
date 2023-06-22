233de5eb228199b35a79ac2f4a995ce914c89eec
Toby Liu <ybot1122@gmail.com>
2023-06-22 13:55:23 -0700

    Add steps to connect Social Media

--- src/components/chefOnboardingSteps/stepTwo/chefOnboardingStepTwo.tsx
+++ src/components/chefOnboardingSteps/stepTwo/chefOnboardingStepTwo.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const ChefOnboardingStepTwo = () => {
-  return <div>Step Two</div>;
+  return (
+    <div>
+      <h2>Link Your Social Media</h2>
+
+      <div>Connect your Facebook</div>
+
+      <div>Connect your Instagram</div>
+
+      <div>Connect your TikTok</div>
+
+      <div>Connect your Youtube</div>
+
+      <div>Any other Social Media?</div>
+    </div>
+  );
 };
 
 export { ChefOnboardingStepTwo };
