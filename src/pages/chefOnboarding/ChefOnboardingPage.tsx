291a8b3ea59df59b7f6ce2e761e800f8b09e4aed
Toby Liu <ybot1122@gmail.com>
2023-06-21 20:01:43 -0700

    Add next and previous step buttons

--- src/pages/chefOnboarding/ChefOnboardingPage.tsx
+++ src/pages/chefOnboarding/ChefOnboardingPage.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { ChefOnboardingStepOne } from "../../components/chefOnboardingSteps/stepOne/chefOnboardingStepOne";
 import { ChefOnboardingStepTwo } from "../../components/chefOnboardingSteps/stepTwo/chefOnboardingStepTwo";
 import { ChefOnboardingStepThree } from "../../components/chefOnboardingSteps/stepThree/chefOnboardingStepThree";
 
 const ChefOnboardingPage = () => {
   const [currentStep, setCurrentStep] = React.useState(0);
+
+  const goToNextStep = useCallback(
+    () => setCurrentStep(currentStep + 1),
+    [currentStep]
+  );
+  const goToPrevStep = useCallback(
+    () => setCurrentStep(currentStep - 1),
+    [currentStep]
+  );
 
   return (
     <section>
@@ -21,6 +30,13 @@ const ChefOnboardingPage = () => {
         {currentStep === 0 && <ChefOnboardingStepOne />}
         {currentStep === 1 && <ChefOnboardingStepTwo />}
         {currentStep === 2 && <ChefOnboardingStepThree />}
+
+        <button onClick={goToNextStep} disabled={currentStep === 2}>
+          Go To Next Step
+        </button>
+        <button onClick={goToPrevStep} disabled={currentStep === 0}>
+          Go To Previous Step
+        </button>
       </div>
     </section>
   );
