a97e67716d47789aaf5a32195cf3058eb2cc74b1
Toby Liu <ybot1122@gmail.com>
2023-06-22 14:35:09 -0700

    Pending state for each social media

--- src/components/chefOnboardingSteps/stepTwo/chefOnboardingStepTwo.tsx
+++ src/components/chefOnboardingSteps/stepTwo/chefOnboardingStepTwo.tsx
@@ -2,42 +2,69 @@ import React from "react";
 
 import * as css from "./chefOnboardingStepTwo.css";
 
+type ConnectedStatus = 0 | 1 | -1; // 0 = not connected, 1 = connected, -1 = pending
+
 const ChefOnboardingStepTwo = () => {
-  const onFacebookConnect = React.useCallback(
-    () => setTimeout(() => console.log("fb"), 1000),
-    []
-  );
-  const onInstagramConnect = React.useCallback(
-    () => setTimeout(() => console.log("fb"), 1000),
-    []
-  );
-  const onTikTokConnect = React.useCallback(
-    () => setTimeout(() => console.log("fb"), 1000),
-    []
-  );
-  const onYoutubeConnect = React.useCallback(
-    () => setTimeout(() => console.log("fb"), 1000),
-    []
-  );
+  const [facebookConnected, setFacebookConnected] =
+    React.useState<ConnectedStatus>(0);
+  const [instagramConnected, setInstagramConnected] =
+    React.useState<ConnectedStatus>(0);
+  const [tikTokConnected, setTikTokConnected] =
+    React.useState<ConnectedStatus>(0);
+  const [youtubeConnected, setYoutubeConnected] =
+    React.useState<ConnectedStatus>(0);
+
+  const onFacebookConnect = React.useCallback(() => {
+    setFacebookConnected(-1);
+    setTimeout(() => setFacebookConnected(1), 1000);
+  }, [setFacebookConnected]);
+  const onInstagramConnect = React.useCallback(() => {
+    setInstagramConnected(-1);
+    setTimeout(() => setInstagramConnected(1), 1000);
+  }, [setInstagramConnected]);
+  const onTikTokConnect = React.useCallback(() => {
+    setTikTokConnected(-1);
+    setTimeout(() => setTikTokConnected(1), 1000);
+  }, [setTikTokConnected]);
+  const onYoutubeConnect = React.useCallback(() => {
+    setYoutubeConnected(-1);
+    setTimeout(() => setYoutubeConnected(1), 1000);
+  }, [setYoutubeConnected]);
 
   return (
     <div>
       <h2>Link Your Social Media</h2>
 
       <div className={css.connectButton} onClick={onFacebookConnect}>
-        Connect your Facebook
+        {facebookConnected === 1
+          ? "Facebook Connected!"
+          : facebookConnected === 0
+          ? "Connect your Facebook"
+          : "pending"}
       </div>
 
       <div className={css.connectButton} onClick={onInstagramConnect}>
-        Connect your Instagram
+        {instagramConnected === 1
+          ? "Instagram Connected!"
+          : instagramConnected === 0
+          ? "Connect your Instagram"
+          : "pending"}
       </div>
 
       <div className={css.connectButton} onClick={onTikTokConnect}>
-        Connect your TikTok
+        {tikTokConnected === 1
+          ? "TikTok Connected!"
+          : tikTokConnected === 0
+          ? "Connect your TikTok"
+          : "pending"}
       </div>
 
       <div className={css.connectButton} onClick={onYoutubeConnect}>
-        Connect your Youtube
+        {youtubeConnected === 1
+          ? "Youtube Connected!"
+          : youtubeConnected === 0
+          ? "Connect your Youtube"
+          : "pending"}
       </div>
 
       <div className={css.connectButton}>Any other Social Media?</div>
