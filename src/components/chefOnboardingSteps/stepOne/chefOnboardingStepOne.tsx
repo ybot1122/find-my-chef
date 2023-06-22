d8edf31bda99811f3ff74a7382a092b95b66cfb1
Toby Liu <ybot1122@gmail.com>
2023-06-22 11:40:51 -0700

    Add options to select your cuisines

--- src/components/chefOnboardingSteps/stepOne/chefOnboardingStepOne.tsx
+++ src/components/chefOnboardingSteps/stepOne/chefOnboardingStepOne.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const ChefOnboardingStepOne = () => {
-  return <div>Step One</div>;
+  return (
+    <div>
+      <h2>Select Your Cuisines</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <td>American</td>
+            <td>Mexican</td>
+            <td>Chinese</td>
+            <td>Italian</td>
+          </tr>
+          <tr>
+            <td>Nigerian</td>
+            <td>Japanese</td>
+            <td>Korean</td>
+            <td>Argentinian</td>
+          </tr>
+          <tr>
+            <td>Spanish</td>
+            <td>British</td>
+            <td>Mediterranean</td>
+            <td>Indian</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export { ChefOnboardingStepOne };
